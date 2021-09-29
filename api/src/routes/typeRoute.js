42e824bdeefcbbf3c8d90cd283c285c0ea6fc2a5
David Heredia <davis_heredia@hotmail.com>
2021-09-28 23:30:17 -0300

    Finish

---
+++ api/src/routes/typeRoute.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const axios = require('axios');
+const router = express.Router();
+const { Types } = require('../db');
+
+router.get('/', async (req, res, next) => {
+        try {
+                const dbTypes = await Types.findAll();
+                if (!dbTypes.length) {
+                        const apiTypes = (await axios.get("https://pokeapi.co/api/v2/type")).data.results;
+                        const types = apiTypes.map(g => ({ name: g.name }));
+                        const dbtypesCreate = await Types.bulkCreate(types)
+                        return res.send(dbtypesCreate)
+
+                } else {
+                        return res.status(201).json(dbTypes)
+                }
+        } catch (err) {
+                next(err);
+        }
+});
+
+module.exports = router;
