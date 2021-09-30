aaf5b1ff26a7338e949bec21ed645e65c7be5b89
David Heredia <davis_heredia@hotmail.com>
2021-09-30 16:27:44 -0300

    Fin

--- api/src/models/Types.js
+++ api/src/models/Types.js
@@ -4,11 +4,6 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
     // defino el modelo
     sequelize.define('types', {
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true
-        },
         name: {
             type: DataTypes.STRING
         },
